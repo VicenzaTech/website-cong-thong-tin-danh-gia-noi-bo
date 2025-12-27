c579898e886e8e5abf292f991d6bcd3746687b0c
Nguyen Quang Hung <121934030+nqhung119@users.noreply.github.com>
2025-12-27 08:16:26 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="vi" style={{ width: "100%", maxWidth: "100%", overflowX: "hidden" }}>
+    <html lang="vi" suppressHydrationWarning style={{ width: "100%", maxWidth: "100%", overflowX: "hidden" }}>
       <head>
         <ColorSchemeScript />
       </head>
